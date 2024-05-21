c67e07048c93f5e849d9ff8ebd49bc37c25cec08
drabo <machine9698@gmail.com>
2024-05-21 15:35:17 +0000

    photo

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Mamadou TRAORE',
     title: "Developpeur Web",
     desciption:"La programmation, c'est comme une symphonie. Il faut que tous les instruments jouent ensemble pour créer quelque chose d'extraordinaire.\nChaque grand projet a commencé par une ligne de code.",
-    image: 'https://scontent.fabj4-2.fna.fbcdn.net/v/t39.30808-6/376845285_817439556592553_7530707396659800780_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFk8P8zL2pJs00wpAr26M9cQ1sigoiD9QhDWyKCiIP1CKu6_-fdhK_tsJcCNi1p-je9rYBlOPBXjvDKPmuyB78b&_nc_ohc=NqzOIbsQe9cAX9_XGgH&_nc_ht=scontent.fabj4-2.fna&oh=00_AfBL6poDFYpG21UkPPsd1Nzl2balX5u4i9IA4mvoBIon6g&oe=6606B499',
+    image: 'https://scontent.fabj4-2.fna.fbcdn.net/v/t39.30808-6/376845285_817439556592553_7530707396659800780_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFk8P8zL2pJs00wpAr26M9cQ1sigoiD9QhDWyKCiIP1CKu6_-fdhK_tsJcCNi1p-je9rYBlOPBXjvDKPmuyB78b&_nc_ohc=8uRaUqYAZKgQ7kNvgFEX4FM&_nc_ht=scontent.fabj4-2.fna&oh=00_AYAcyd5cN7XZ0s-GmkUny2dCZQ1D3Sqc-lor1Hjasf906Q&oe=665282D9',
     resumePdf: resume
 }
